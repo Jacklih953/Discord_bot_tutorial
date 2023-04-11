6532a6666f39c693033d0a299d7f2470ac4446d8
Jacklih953 <85166729+Jacklih953@users.noreply.github.com>
2023-04-12 00:20:40 +0800

    Add files via upload

--- bot_test.py
+++ bot_test.py
@@ -1,39 +1,31 @@
 import discord
 import openai
-from collections import deque
+from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content =True
-client = discord.client(intents=intents)
-
-dc_bot_token = ""
-
-@client.event
-async def on_ready():
-    print("bot is ready")
-
+dc_bot_token = "MTA5NTMzNzQ3ODk3NTY3MjM2Mg.Glimpa.SHyTFnBOTlJiWWCh5VsN2PBybhC2vhfZm5ZUxw"
 
 messages = [{"role": "system", "content": "You are a helpful assistant"}]
 openai.api_key = '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'
 
+intents = discord.Intents.default()
+intents.message_content =True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(client.user, "bot is ready！！")
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
-          return
-    await message.channel.send(message)
+        return
+    user_input = message.content
+    messages.append({"roel": "user", "content": user_input})
+    completion = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = messages
+        )
+    chat_response= completion.choices[0].message.content
+    await message.channel.send(chat_response)
 
-    
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     messages.append({"role": "assistant", "content": message})
-#     completion = openai.ChatCompletion.create(
-#         model = 'gpt-3.5-turbo',
-#         messages = messages
-#     )
-#     chat_response = completion.choices[0].message.content
-#     await message.channel.send(chat_response)
-    
-
-
-
+client.run(dc_bot_token)
